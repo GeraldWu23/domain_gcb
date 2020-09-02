10892d7baa347ee9b4a438e84697431e02ac55df
Gerald Wu <wukakit96@gmail.com>
2020-09-02 15:41:49 +0800

    StrictHub recognises Chinese names with noise

--- StrictHub.py
+++ StrictHub.py
@@ -1,9 +1,7 @@
 import re
-
 import requests
-
 from utilities import isname, findChinese
-
+from time import time
 
 def isStrictHub(html, threshold=5):
     """
@@ -14,23 +12,36 @@ def isStrictHub(html, threshold=5):
     :return: True if it's considered a strict hub page
     """
 
+    html = html.lower()
     html_clean = ' '.join(html.split())
     astring = re.findall(r'<a.*?>.*?</a>', html_clean)
     names = set()
+    textlist = []
 
     for ahref in astring:  # for all <a></a>
-        textlist = [findChinese(string) for string in re.findall(r'>.+?<', ahref) if string != '> <']  # content list in this <a></a>
-        if textlist:
-            textonlink = textlist[-1].strip('> <')  # name might appear more than one time
-            if isname(textonlink):  # the content is a Chinese name
-                names.add(textonlink)
-                print(textonlink)
-            else:
-                print(f'not link: {textonlink}')
+        for string in re.findall(r'>.+?<', ahref):
+            Chineseintext = findChinese(string)
+            if Chineseintext:
+                if len(Chineseintext) <= 4:  # e.g. 葛昌纯1968年， 院士-张清杰，院士-张-联盟
+                    textlist.extend(Chineseintext)
 
-    return len(names) > threshold  # page is considered a strict hub page when number of unique names is larger than threshold
+    # textlist = [findChinese(string) for string in re.findall(r'>.+?<', ahref) if string != '> <' or string != '']
+    textlist = list(set([i for i in textlist if i]))
+    print(textlist)
+    for textonlink in textlist:
+        if isname(textonlink):  # the content is a Chinese name
+            names.add(textonlink)
+            print(textonlink)
+        else:
+            print(f'not link: {textonlink}')
+
+    return len(names) >= threshold  # page is considered a strict hub page when number of unique names is larger than threshold
 
 
 if __name__ == '__main__':
-    html = str(requests.get('https://mse.ustc.edu.cn/3334/list.htm').content, encoding='utf-8')
+    starttime = time()
+    html = str(requests.get('https://chem.nwnu.edu.cn/2017/0322/c373a20293/page.htm').content, encoding='utf-8')
     print(isStrictHub(html))
+
+    endtime = time()
+    print(f'used time is {endtime - starttime}')
--- utilities.py
+++ utilities.py
@@ -29,23 +29,26 @@ def findChinese(text):
     '''
     find all Chinese characters and join them to a string
 
-    :param text:
+    :param text
     :return: a string representing all Chinese
     '''
 
     chinese_pattern = re.compile(u'[\u4e00-\u9fa5]')
     res =  ''.join(re.findall(chinese_pattern, text))
-    return res
+    if not res:
+        return None
+    words = pseg.cut(res)
+    return [w.word for w in words]
 
 
 
+if __name__ == '__main__':
 
+    p1 = '帮会建了徽信群 没在群里的加下徽信:[30109552300]，晚上群里有活动通知大家，(抢资源)，争地盘，谢谢配合。i love you '
+    p2 = '<a><img>sdfasgdfgf</img><div>sdfsdfsdfasdgf</div><span>徐璋勇</span></a>'
 
-p1 = '帮会建了徽信群 没在群里的加下徽信:[30109552300]，晚上群里有活动通知大家，(抢资源)，争地盘，谢谢配合。i love you '
-p2 = '<a><img>sdfasgdfgf</img><div>sdfsdfsdfasdgf</div><span>徐璋勇</span></a>'
-
-pre = re.compile(u'[\u4e00-\u9fa5]')
-res = re.findall(pre, p2)
-res1 = ''.join(res)
-print(res1)
+    pre = re.compile(u'[\u4e00-\u9fa5]')
+    res = re.findall(pre, p2)
+    res1 = ''.join(res)
+    print(res1)
 
